d616bfb602ef95747ab5bc98bce5a034315ef689
Victor moreau <victor.moreau@sogeti.com>
2018-03-12 17:41:01 +0100

    Adding Folder && Clean Code

---
+++ SpotBar/SpotBar/server/routes/connectDB.route.js
@@ -0,0 +1,30 @@
+var express = require('express');
+    MongoClient = require('mongodb').MongoClient,
+    format = require('util').format;
+    mongoose = require('mongoose');
+    bcrypt = require('bcrypt');
+
+
+
+
+module.exports.publicRoutes = function () {
+
+    mongoose.connect('mongodb://localhost/spotbardb', function(err) {
+        if (err) { throw err; }
+        else{
+            console.log("database Connected");
+        }
+      });
+
+    
+    var router = express.Router();
+
+    return router;
+}
+module.exports.privateRoutes = function () {
+
+    var router = express.Router();
+
+    return router;
+
+}
---
+++ SpotBar/SpotBar/views/userView.swift
@@ -0,0 +1,14 @@
+//
+//  userView.swift
+//  SpotBar
+//
+//  Created by Victor moreau on 12/03/2018.
+//  Copyright © 2018 Victor moreau. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class userView : UIViewController{
+    
+}
